47de87ce31f79c4d6ac5d8a0758023c00aee0150
Branden Weeks <weeks.branden93@gmail.com>
2026-04-27 12:24:07 -0400

    Seperated quiz from main working project file

--- test_sheet.py
+++ test_sheet.py
@@ -1,4 +1,4 @@
-'''import numpy as np
+import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.impute import SimpleImputer
@@ -29,37 +29,4 @@ from sklearn.preprocessing import LabelEncoder
 label_encoder = LabelEncoder()
 y = label_encoder.fit_transform(y)
 
-print(y)'''
-
-
-# Importing the necessary libraries
-import pandas as pd 
-import numpy as np
-from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.preprocessing import LabelEncoder
-
-# Load the dataset
-dataset = pd.read_csv('titanic.csv')
-
-# Identify the categorical data
-categorical_features = ["Sex", "Embarked", "Pclass"]
-X_columns = ['Sex', 'Embarked', 'Pclass', 'Age', 'SibSp', 'Parch', 'Fare']
-X = dataset[X_columns]
-
-# Implement an instance of the ColumnTransformer class
-transformer = ColumnTransformer(transformers=[("encoder", OneHotEncoder(), categorical_features)], remainder = "passthrough")
-
-# Apply the fit_transform method on the instance of ColumnTransformer
-X = transformer.fit_transform(X)
-
-# Convert the output into a NumPy array
-X = np.array(X)
-
-# Use LabelEncoder to encode binary categorical data
-label_encoder = LabelEncoder()
-y = label_encoder.fit_transform(dataset['Survived'])
-
-# Print the updated matrix of features and the dependent variable vector
-print(X)
 print(y)
