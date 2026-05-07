3a62fcbe411bcce8e7704c973fbd0c8ac93dcf68
Branden Weeks <weeks.branden93@gmail.com>
2026-05-06 20:30:09 -0400

    Folder restructure for polynomial regression

---
+++ Machine-Learning-A-Z-Codes-Datasets/Part 2 - Regression/Section 5 - Multiple Linear Regression/Python/50_Startups.py
@@ -0,0 +1,28 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+dataset = pd.read_csv('50_Startups.csv')
+x = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+transformer = ColumnTransformer(transformers=[("encoder", OneHotEncoder(), [-1])], remainder="passthrough")
+x = np.array(transformer.fit_transform(x))
+
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 1)
+
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(x_train, y_train)
+
+y_pred = regressor.predict(x_test)
+np.set_printoptions(precision=2)
+print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test), 1)), 1))
+
+#But how was this model able to make determinations based on the IV's? 
+#How did it learn what it learned?
+#What did it learn?
