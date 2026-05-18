e5927d6b2d5393cf611e140200e715e9c925e284
Branden Weeks <weeks.branden93@gmail.com>
2026-05-18 14:44:30 -0400

    Random Forest/Model Selection

--- Machine-Learning-A-Z-Codes-Datasets/Part 2 - Regression/Section 9 - Random Forest Regression/Python/RF_regression.py
+++ Machine-Learning-A-Z-Codes-Datasets/Part 2 - Regression/Section 9 - Random Forest Regression/Python/RF_regression.py
@@ -1,17 +1,16 @@
+import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import pandas as pd
 
 dataset = pd.read_csv('Position_Salaries.csv')
 x = dataset.iloc[:, 1:-1].values
 y = dataset.iloc[:, -1].values
 
-from sklearn.tree import DecisionTreeRegressor
-regressor = DecisionTreeRegressor(random_state = 0)
+from sklearn.ensemble import RandomForestRegressor
+regressor = RandomForestRegressor(n_estimators=500, random_state=1234)
 regressor.fit(x, y)
 
-#print(regressor.predict([[6.5]]))
-#rememeber that this model is not effective for simple datasets with only 2 dimensions
+print(regressor.predict([[6.5]]))
 
 x_grid = np.arange(x.min(), x.max(), 0.1)
 x_grid = x_grid.reshape((len(x_grid), 1))
